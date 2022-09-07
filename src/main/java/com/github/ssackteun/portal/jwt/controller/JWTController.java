e477420b027118445a67d7142c3c57b3dd0da87e
macbookair <jaeyoung645@naver.com>
2022-09-08 00:17:50 +0900

    feat : "ADD Token.java"
    made Token Class

---
+++ src/main/java/com/github/ssackteun/portal/jwt/controller/JWTController.java
@@ -0,0 +1,8 @@
+package com.github.ssackteun.portal.jwt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JWTController {
+
+}
---
+++ src/main/java/com/github/ssackteun/portal/jwt/token/Token.java
@@ -0,0 +1,10 @@
+package com.github.ssackteun.portal.jwt.token;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Token {
+    private String Token;
+}
