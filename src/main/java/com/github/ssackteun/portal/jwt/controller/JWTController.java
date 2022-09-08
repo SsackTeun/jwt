14095531b020028a29aee3cada65be25cf35d4b1
macbookair <jaeyoung645@naver.com>
2022-09-08 10:18:52 +0900

    TEST : "JWTControllerTests.java --> method getToken() passed"

--- src/main/java/com/github/ssackteun/portal/jwt/controller/JWTController.java
+++ src/main/java/com/github/ssackteun/portal/jwt/controller/JWTController.java
@@ -1,8 +1,21 @@
 package com.github.ssackteun.portal.jwt.controller;
 
+import com.github.ssackteun.portal.jwt.token.IdPasswordLogin;
+import com.github.ssackteun.portal.jwt.token.Token;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JWTController {
+
+    @GetMapping("/auth/token")
+    public ResponseEntity<Token> getToken(@RequestBody IdPasswordLogin idPasswordLogin){
+        Token jwt = new Token();
+        jwt.setToken("test");
+        return new ResponseEntity<>(jwt, HttpStatus.CREATED);
+    }
 
 }
---
+++ src/test/java/com/github/ssackteun/portal/jwt/JWTControllerTests.java
@@ -0,0 +1,39 @@
+package com.github.ssackteun.portal.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@SpringBootTest
+public class JWTControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void token() throws Exception{
+        this.mockMvc.perform(get("/auth/token")
+                .content("{\"userid\": \"ssackteun\",\n\"password\": \"asdf\" }")
+                .contentType(MediaType.APPLICATION_JSON)) //요청
+
+                .andExpect(status().isCreated()) //검증
+                .andDo(document("get-token",
+                            requestFields(
+                                    fieldWithPath("userid").description("유저 아이디"),
+                                    fieldWithPath("password").description("유저 패스워드").optional()
+                            )
+                ))
+                .andDo(print());
+    }
+}
