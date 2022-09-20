5a2aa250b34eb12c7275d41bee257ee60fb7ee5d
macbookair <jaeyoung645@naver.com>
2022-09-20 10:59:44 +0900

    fix(CustomUserDetailsService.java) : customUserDetail 메서드 수정
    modify : customUserDetails return User -> return CustomUserDetails

--- src/main/java/com/github/ssackteun/portal/jwt/service/auth/CustomUserDetailsService.java
+++ src/main/java/com/github/ssackteun/portal/jwt/service/auth/CustomUserDetailsService.java
@@ -5,12 +5,12 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.github.ssackteun.portal.jwt.entity.CustomUserDetails;
 import com.github.ssackteun.portal.jwt.entity.Member;
 import com.github.ssackteun.portal.jwt.repository.AuthRepository;
 
@@ -40,6 +40,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private UserDetails customUserDetails(Member member){
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
-        return new User(String.valueOf(member.getUserId()), member.getPassword(), Collections.singleton(grantedAuthority) );
+        return new CustomUserDetails(
+            member.getUserId(),
+            member.getPassword(),
+            member.getEmail(),
+            member.getName(),
+            Collections.singleton(grantedAuthority));
     }
 }
